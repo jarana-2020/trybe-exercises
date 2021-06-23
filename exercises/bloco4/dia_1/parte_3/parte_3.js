49fd92f9a1da1960daa2a221928d9bb709643ba3
jarana-2020 <julioaranabr@yahoo.com.br>
2021-06-23 16:08:34 -0300

    Requisitos #1 #2 #3

---
+++ exercises/bloco4/dia_1/parte_3/parte_3.js
@@ -0,0 +1,9 @@
+let base = 5;
+let altura = 8;
+let area = base * altura;
+let perimetro = base + altura + altura;
+
+console.log(base);
+console.log(altura);
+console.log(area);
+console.log(perimetro);

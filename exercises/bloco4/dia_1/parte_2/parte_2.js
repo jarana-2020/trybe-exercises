b3f230a7aed45e327f65b8555a42d4226e508bce
jarana-2020 <julioaranabr@yahoo.com.br>
2021-06-23 15:56:01 -0300

    refatoracao para concerto do requisito #6

--- exercises/bloco4/dia_1/parte_2/parte_2.js
+++ exercises/bloco4/dia_1/parte_2/parte_2.js
@@ -2,8 +2,6 @@ const name = "Julio Cesar Arana";
 const birthCity = "Londrina";
 let birthYear = 2300;
 
-birthCity = "Curitiba"
-
 console.log(name);
 console.log(birthCity);
 console.log(birthYear);

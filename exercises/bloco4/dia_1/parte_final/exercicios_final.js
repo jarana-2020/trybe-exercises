be2a7e42a69e8472a6011fd13cd06c1c73209fcf
jarana-2020 <julioaranabr@yahoo.com.br>
2021-06-23 19:20:11 -0300

    Requisitos #4 #5 #6 #7

--- exercises/bloco4/dia_1/parte_final/exercicios_final.js
+++ exercises/bloco4/dia_1/parte_final/exercicios_final.js
@@ -7,6 +7,13 @@ let multiplicação = a * b;
 let divisão = a / b;
 let mod = a % b;
 let maior;
+let verificaPositivo = 80;
+let angulo1 = 40;
+let angulo2 = 60;
+let angulo3 = 80;
+let somaAngulos = angulo1 + angulo2 + angulo3;
+let pecaXadrez = "Bishop";
+let grade = 69;
 
 if (a > b && a > c){
     maior = a;
@@ -15,6 +22,54 @@ if (a > b && a > c){
 }else{
     maior = c;
 }
+
+if (verificaPositivo < 0){
+    console.log("negativo");
+}else if (verificaPositivo > 0){
+    console.log("positivo");
+}else{
+    console.log("zero");
+}
+if(somaAngulos == 180 && angulo1 > 0 && angulo2 > 0 && angulo3 > 0){
+    console.log("true")
+}else if (somaAngulos != 180 && angulo1 > 0 && angulo2 > 0 && angulo3 > 0){
+    console.log("false")
+}else{
+    console.log("Um dos angulos do triangulo é negativo, favor verificar");
+}
+
+switch(pecaXadrez.toLowerCase()){
+    case "bishop":
+        console.log("Movimento da peça é diagonal");
+        break;
+    case "king":
+        console.log("Movivento da peça é (horizontal, vertical e diagonal)");
+        break;
+    case "queen":
+        console.log("Movimento da peça é ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.");
+        break;
+    default:
+        console.log("A peça informada não valida !!!");            
+}
+ 
+let grade = 76;
+
+if (grade < 0 || grade > 100) {
+  console.log("Erro, nota incorreta!");
+} else if (grade >= 90) {
+  console.log("A");
+} else if (grade >= 80) {
+  console.log("B");
+} else if (grade >= 70) {
+  console.log("C");
+} else if (grade >= 60) {
+  console.log("D");
+} else if (grade >= 50) {
+  console.log("E");
+} else {
+  console.log("F");
+}  
+
 
 
 console.log(adição);

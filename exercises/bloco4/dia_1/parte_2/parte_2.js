78edadf96fa21c8addc0451ace291abbefbad4d5
jarana-2020 <julioaranabr@yahoo.com.br>
2021-06-23 15:49:44 -0300

    exercicios #3 e #4

--- exercises/bloco4/dia_1/parte_2/parte_2.js
+++ exercises/bloco4/dia_1/parte_2/parte_2.js
@@ -1,2 +1,7 @@
-const name = "Julio Cesar Arana"
-const birthCity = "Londrina"
+const name = "Julio Cesar Arana";
+const birthCity = "Londrina";
+let birthYear = 1982;
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);

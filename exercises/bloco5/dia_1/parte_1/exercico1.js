4b97195934001ed20cfc80c95e28391f8d4793ff
jarana-2020 <julioaranabr@yahoo.com.br>
2021-06-30 18:00:41 -0300

    Exercicio de fixaçao Administrador de Tempo

--- exercises/bloco5/dia_1/parte_1/exercico1.js
+++
@@ -1,5 +0,0 @@
-const paragraph = document.getElementById("paragraph");
-paragraph.style.color = "red";
-document.getElementById('page-title').innerText = "Batman";
-document.getElementById('second-paragraph').innerText = "Julio Cesar Arana";
-document.getElementById('subtitle').innerText = "Ana Carolina";

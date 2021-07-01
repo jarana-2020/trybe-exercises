da18367f1340e5bb641c33976bb7e5216b5175ba
jarana-2020 <julioaranabr@yahoo.com.br>
2021-06-30 21:43:29 -0300

    Requisito #2 e #3  DOM

--- exercises/bloco5/dia_1/parte_final/Exerecicios_DOM/exercicio_dom.js
+++ exercises/bloco5/dia_1/parte_final/Exerecicios_DOM/exercicio_dom.js
@@ -3,3 +3,16 @@ function alterText(){
     alterP.innerHTML = 'Rico';
 }
 alterText();
+
+function alterMainContent(){
+    let alterMain = document.getElementsByClassName('main-content')[0];
+    alterMain.style.background = 'rgb(76,164,109';
+}
+alterMainContent();
+
+function alterMainCenter(){
+    let alterCenter = document.getElementsByClassName('center-content')[0];
+    alterCenter.style.background = 'white';
+}
+alterMainCenter();
+

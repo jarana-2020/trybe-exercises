9a60659578ae9e47b85b7fd36d0669597091a615
jarana-2020 <julioaranabr@yahoo.com.br>
2021-07-05 20:07:27 -0300

    Requisito # 1 parte 2

---
+++ exercises/bloco5/dia_2/exercicio1.js
@@ -0,0 +1,4 @@
+const elementFather = document.querySelector('#pai');
+const brotherElement = document.createElement('div');
+brotherElement.id = 'brotherElementoOndeVoceEsta';
+elementFather.appendChild(brotherElement);

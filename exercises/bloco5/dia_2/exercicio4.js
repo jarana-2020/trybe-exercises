942913c54847dc3a7a6c9e7ca870a8dc62e6d2b8
jarana-2020 <julioaranabr@yahoo.com.br>
2021-07-05 21:05:57 -0300

    Requisito #4 parte 2

--- exercises/bloco5/dia_2/exercicio4.js
+++ exercises/bloco5/dia_2/exercicio4.js
@@ -2,3 +2,5 @@ const getFirstChildOfChild = document.querySelector('#primeiroFilhoDoFilho');
 const setChildFirstChildOfChild = document.createElement('div');
 setChildFirstChildOfChild.id = 'ChildFirstChildOfChild';
 getFirstChildOfChild.appendChild(setChildFirstChildOfChild);
+
+setChildFirstChildOfChild.parentElement.parentElement.nextElementSibling;

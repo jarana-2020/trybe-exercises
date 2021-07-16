d2d6a18dd665e96f4973d275fe3341aa92e0ca18
jarana-2020 <julioaranabr@yahoo.com.br>
2021-07-16 20:44:25 -0300

    Finalização dos exercicios Bloco 6 dia 2.

--- exercises/bloco6/dia_2/script.js
+++ exercises/bloco6/dia_2/script.js
@@ -1,19 +1,30 @@
+import './node_modules/just-validate/dist/js/just-validate.js'
+
 const states = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
-let arrayMessages = [];
-let arrayErrors = [];
-const getForm = document.getElementById('main-form');
-const divValues = document.getElementById('div-result');
 
-
-window.onload = function () {
-    const getButtonSubmit = document.querySelector('.button-submit');
-    getButtonSubmit.addEventListener('click', handleSubmit);
-    const getButtonClear = document.querySelector('.button-clear');
-    getButtonClear.addEventListener('click', clearForm);
-}
+const picker = new Pikaday({
+    field: document.getElementById('datepicker'),
+    format: 'D/M/YYYY',
+    toString(date, format) {
+      // you should do formatting based on the passed format,
+      // but we will just return 'D/M/YYYY' for simplicity
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+      return `${day}/${month}/${year}`;
+    },
+    parse(dateString, format) {
+      // dateString is the result of `toString` method
+      const parts = dateString.split('/');
+      const day = parseInt(parts[0], 10);
+      const month = parseInt(parts[1], 10) - 1;
+      const year = parseInt(parts[2], 10);
+      return new Date(year, month, day);
+    }
+  });
 
 function getStates() {
-    const getSelect = document.getElementById('select-state');
+    const getSelect = document.getElementById('state');
     for (let index = 0; index < states.length; index += 1) {
         const getOption = document.createElement('option');
         getOption.className = 'option';
@@ -23,182 +34,98 @@ function getStates() {
 }
 getStates();
 
-function validateName() {
-    const getName = document.querySelector('#input-name');
-    if (getName.value !== '') {
-        arrayMessages.push('Nome:' + getName.value);
-    } else {
-        arrayErrors.push('Campo Nome deve ser preenchido.');
-    }
-};
-
-function validateEmail() {
-    const getEmail = document.querySelector('#input-email');
-    if (getEmail.value !== '') {
-        arrayMessages.push('E-mail:' + getEmail.value);
-    } else {
-        arrayErrors.push('Campo E-mail deve ser preenchido.');
-    }
-};
-
-function validateCpf() {
-    const getCpf = document.querySelector('#input-cpf');
-    if (getCpf.value !== '') {
-        arrayMessages.push('Cpf:' + getCpf.value);
-    } else {
-        arrayErrors.push('Campo Cpf deve ser preenchido.');
-    }
-};
-
-function validateAdress() {
-    const getAdress = document.querySelector('#input-adress');
-    if (getAdress.value !== '') {
-        arrayMessages.push('Endereço:' + getAdress.value);
-    } else {
-        arrayErrors.push('Campo Endereço deve ser preenchido.');
-    }
-};
-
-function validateCity() {
-    const getCity = document.querySelector('#input-city');
-    if (getCity.value !== '') {
-        arrayMessages.push('Cidade:' + getCity.value);
-    } else {
-        arrayErrors.push('Campo Cidade deve ser preenchido.');
-    }
-};
-
-function validateState() {
-    const getState = document.querySelector('#select-state');
-    if (getState.selectedIndex !== 0) {
-        arrayMessages.push('Estado:' + getState.value);
-    } else {
-        arrayErrors.push('Você deve seleconar o Estado.');
-    }
-};
-
-function validateHouse() {
-    const getHouse = document.getElementsByName('input-home');
-    if (getHouse[0].checked) {
-        arrayMessages.push('Tipo de Moradia:' + getHouse[0].value);
-        console.log(arrayMessages);
-    } else if (getHouse[1].checked) {
-        arrayMessages.push('Tipo de Moradia:' + getHouse[1].value);
-    } else {
-        arrayErrors.push('Você deve seleconar o tipo de moradia.')
-    }
-};
-
-function validateResume() {
-    const getResume = document.querySelector('#resume-cv');
-    if (getResume.value !== "") {
-        arrayMessages.push('Resumo do Curriculo:' + getResume.value);
-    } else {
-        arrayErrors.push('Você deve preencher o resumo do curriculo.');
-    }
-};
-
-function validateOffice() {
-    const getOffice = document.querySelector('#input-office');
-    if (getOffice.value !== "") {
-        arrayMessages.push('Resumo do Curriculo:' + getOffice.value);
-    } else {
-        arrayErrors.push('Você deve preencher o cargo.');
-    }
-};
-
-function validateEmail() {
-    const getEmail = document.querySelector('#input-email');
-    if (getEmail.value !== '') {
-        arrayMessages.push('Resumo do Curriculo:' + getDescription.value);
-    } else {
-        arrayErrors.push('Você deve preencher a descrição do cargo.');
-    }
-};
-
-function validateDate() {
-
-    const getDate = document.getElementById('input-date');
-    let regex = /^\d\d\/\d\d\/\d\d\d\d$/;
-    let separetor = getDate.value.split('/');
-    let day = separetor[0];
-    let month = separetor[1];
-    let year = separetor[2];
-
-    if (getDate.value === '') {
-        arrayErrors.push('A data deve ser informada');
-    } else if (!regex.test(getDate.value)) {
-        arrayErrors.push('Data no formato invalido');
-    } else if (day < 0 || day > 30) {
-        arrayErrors.push('O dia informado não é valido');
-    } else if (month < 1 || month > 12) {
-        arrayErrors.push('O mês informado não é valido');
-    } else if (year < 0) {
-        arrayErrors.push('O ano informado não é valido');
-    } else {
-        arrayMessages.push('Data: ' + getDate.value);
-    }
-
-}
-
-function clearDivResult() {
-    while (divValues.firstChild) {
-        divValues.removeChild(divValues.firstChild);
-    }
-}
-
-
-function handleSubmit(event) {
-    event.preventDefault()
-    arrayErrors = [];
-    arrayMessages = [];
-    validateName();
-    validateEmail();
-    validateCpf();
-    validateAdress();
-    validateCity();
-    validateState();
-    validateHouse();
-    validateResume();
-    validateOffice();
-    validateDescription();
-    validateDate();
-
-    if (arrayErrors.length > 0) {
-        arrayMessages = [];
-        clearDivResult();
-        for (let index = 0; index < arrayErrors.length; index += 1) {
-            const getP = document.createElement('p');
-            getP.innerHTML = arrayErrors[index];
-            divValues.appendChild(getP);
-            divValues.style.backgroundColor = 'red';
-            getForm.appendChild(divValues);
-        }
-    } else {
-        clearDivResult();
-        for (let index = 0; index < arrayMessages.length; index += 1) {
-            const getP = document.createElement('p');
-            getP.innerHTML = arrayMessages[index];
-            divValues.appendChild(getP);
-            divValues.style.backgroundColor = 'cyan';
-            getForm.appendChild(divValues);
-        }
-    }
-
-};
-
-function clearForm(event) {
-    event.preventDefault()
-    const getInputs = document.querySelectorAll('.input');
-    const getTextArea = document.querySelector('#resume-cv');
-    const inputRadio = document.getElementsByName('input-home');
-    for (let index = 0; index < getInputs.length; index += 1) {
-        getInputs[index].value = '';
-    }
-    clearDivResult();
-    getTextArea.value = "";
-
-    for (let index = 0; index < inputRadio.length; index += 1) {
-        inputRadio[index].checked = false;
-    }
-}
+new JustValidate('.js-form', {
+    rules: {
+      name: {
+        required: true,
+        minLength: 3,
+        maxLength: 40
+      },
+      email: {
+        required: true,
+        email: true,
+        maxLength: 50
+      },
+      cpf: {
+        required: true,
+        maxLength: 11
+      },
+      address: {
+        required: true,
+        maxLength: 200
+      },
+      city: {
+        required: true,
+        maxLength: 28
+      },
+      state: {
+        required: true,
+        maxLength: 2
+      },
+      radio: {
+        required: true,
+      },
+      text: {
+        required: true,
+        maxLength: 1000
+      },
+      position: {
+        required: true,
+        maxLength: 40
+      },
+      description: {
+        required: true,
+        maxLength: 500
+      },
+      date: {
+        required: true,
+      }
+    },
+    messages: {
+      name: {
+        required: 'O campo de nome é obrigatório.',
+        maxLength: 'O limite é de 40 caracteres.'
+      },
+      email: {
+        required: 'O campo de email é obrigatório.',
+        email: 'O email digitado não é válido.',
+        maxLength: 'O limite é de 50 caracteres.'
+      },
+      cpf: {
+        required: 'O campo de CPF é obrigatório.',
+        maxLength: 'O limite é de 11 caracteres.'
+      },
+      address: {
+        required: 'O campo endereço é obrigatório.',
+        maxLength: 'O limite é de 200 caracteres.'
+      },
+      city: {
+        required: 'O campo cidade é obrigatório.',
+        maxLength: 'O limite é de 28 caracteres.'
+      },
+      state: {
+        required: 'O campo estado é obrigatório.',
+        maxLength: 'Você deve selecionar o estado'
+      },
+      radio: {
+        required: 'A escolha de um item é obrigatória.',
+      },
+      text: {
+        required: 'Este campo é obrigatório.',
+        maxLength: 'O limite é de 1000 caracteres.'
+      },
+      position: {
+        required: 'Este campo é obrigatório.',
+        maxLength: 'O limite é de 40 caracteres.'
+      },
+      description: {
+        required: 'Este campo é obrigatório.',
+        maxLength: 'O limite é de 500 caracteres.'
+      },
+      date: {
+        required: 'Este campo é obrigatório.',
+      }
+    },
+    submitHandler: function (form, values) {
+      console.log(form, values);
+    }});

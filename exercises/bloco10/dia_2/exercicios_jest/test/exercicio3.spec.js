1c4d120d722f42f750fc927236c1e16ba3316857
jarana-2020 <julioaranabr@yahoo.com.br>
2021-08-19 11:25:10 -0300

    Resolvido Exercicio 3

--- exercises/bloco10/dia_2/exercicios_jest/test/exercicio3.spec.js
+++ exercises/bloco10/dia_2/exercicios_jest/test/exercicio3.spec.js
@@ -1,1 +1,17 @@
 const { getUserName } = require('../src/exercicio3');
+
+describe('FindUserById', () => {
+  it('When the user is found', async () => {
+    const getName = await getUserName(1);
+    expect(getName).toBe('Mark');
+  });
+
+  it('When the user does not exists', async () => {
+    expect.assertions(1);
+    try {
+      await getUserName(3); 
+    } catch (error) {
+      expect(error).toEqual(new Error('User with 3 not found.'));  
+    }
+  });
+});

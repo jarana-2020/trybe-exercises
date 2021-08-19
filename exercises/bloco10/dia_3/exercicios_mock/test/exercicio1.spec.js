e94af631106e2c5e1752c41012a5ff31e5ddb88c
jarana-2020 <julioaranabr@yahoo.com.br>
2021-08-19 20:38:25 -0300

    Resolvido Exercicio 1

--- exercises/bloco10/dia_3/exercicios_mock/test/exercicio1.spec.js
+++ exercises/bloco10/dia_3/exercicios_mock/test/exercicio1.spec.js
@@ -1,4 +1,24 @@
-// Crie uma função que gere um número aleatório entre 0 e 100. 
-// Você irá criar também os testes para essa função. Utilizando o mock, 
-// defina o retorno padrão como 10. Teste se a função foi chamada, 
-// qual seu retorno e quantas vezes foi chamada.
+const getNumber = require('../src/exercicio1');
+
+let { generateNumber } = getNumber;
+
+describe('generateNumber', () => {
+  it('Verify if the function was call', () => {
+    generateNumber = jest
+      .fn()
+      .mockReturnValue(10)
+      .mockReturnValueOnce(15)
+      .mockReturnValueOnce(25);
+      
+    expect(generateNumber).toHaveBeenCalledTimes(0);
+
+    expect(generateNumber()).toBe(15);
+    expect(generateNumber).toHaveBeenCalledTimes(1);
+
+    expect(generateNumber()).toBe(25);
+    expect(generateNumber).toHaveBeenCalledTimes(2);
+
+    expect(generateNumber()).toBe(10);
+    expect(generateNumber).toHaveBeenCalledTimes(3);
+  });
+});

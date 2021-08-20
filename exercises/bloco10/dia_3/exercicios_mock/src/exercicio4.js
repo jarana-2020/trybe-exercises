bc2d59e3ca70d0c52e5f9ce55b07ad7ee8bc614f
jarana-2020 <julioaranabr@yahoo.com.br>
2021-08-20 00:49:36 -0300

    Resolvido Exercicio 4

---
+++ exercises/bloco10/dia_3/exercicios_mock/src/exercicio4.js
@@ -0,0 +1,17 @@
+function wordUpperCase(string) {
+  return string.toUppercase();
+}
+
+function firstCharacter(string) {
+  return string.substr(0, 1);
+}
+
+function concatString(string1, string2) {
+  return string1 + string2;
+}
+
+module.exports = {
+  wordUpperCase,
+  firstCharacter,
+  concatString,
+};

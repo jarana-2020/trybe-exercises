f72ef996923ac18fb788eb5f7349d0984b59bedc
jarana-2020 <julioaranabr@yahoo.com.br>
2021-08-31 22:02:06 -0300

    Criado componente Pokemon

---
+++ exercises/bloco11/dia_2/my_pokedex/src/components/pokemon.js
@@ -0,0 +1,20 @@
+import { div } from 'prelude-ls';
+import React from 'react';
+
+
+class Pokemon extends React.Component {
+    render() {
+        const {pokemon: {name, type, averageWeight:
+             { value, measurementUnit}, image }} = this.props;
+        return (
+            <div>
+                <p>{name}</p>
+                <p>{type}</p>
+                <p>{`averageWeight: ${value} ${measurementUnit}`}</p>
+                <img src = {image} alt = {name} />
+            </div>
+        )
+    }
+}
+
+export default Pokemon;

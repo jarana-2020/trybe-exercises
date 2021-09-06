ab9fa7219f9e2dfb074d5df7a0ea3798d5556cde
jarana-2020 <julioaranabr@yahoo.com.br>
2021-09-06 14:33:04 -0300

    Add Proptypes for field name

--- exercises/bloco 12/dia_2/app2_lecture2/src/components/Personal.Fieldset.js
+++ exercises/bloco 12/dia_2/app2_lecture2/src/components/Personal.Fieldset.js
@@ -1,11 +1,30 @@
 import React from 'react';
-
+import PropTypes from 'prop-types'
 class PersonalInformation extends React.Component {
     render() {
+        const { nameValue, handleChange } = this.props
         return(
-
+            <form>
+                <fieldset>
+                    <legend>Personal Information</legend>
+                    <label>
+                        Nome:
+                        <input id='name' name='name' 
+                         type='text' value={nameValue} 
+                         maxLength='40' required='true'
+                         onChange={handleChange}
+                         >
+                        </input>
+                    </label>
+                </fieldset>
+            </form>
         )
     }
 }
 
+
+PersonalInformation.propTypes = {
+    nameValue: PropTypes.string.isRequired,
+    handleChange: PropTypes.func.isRequired,
+}
 export default PersonalInformation;

6d0caa52a8f6f65960d08effe3f767c069f9b281
jarana-2020 <julioaranabr@yahoo.com.br>
2021-09-13 20:53:17 -0300

    Exercise 4

--- exercises/bloco13/dia_2/comprehension-exercises-router-master/src/App.js
+++ exercises/bloco13/dia_2/comprehension-exercises-router-master/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
-import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/" component={ Home } />
-        <Route path="/about" component={ About } />
-      </BrowserRouter>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+    </BrowserRouter>
     );
   }
 }

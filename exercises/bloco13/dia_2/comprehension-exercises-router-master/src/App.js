41da622cbb2f91afa91b0ece4fe567e98b1788ff
jarana-2020 <julioaranabr@yahoo.com.br>
2021-09-13 23:14:52 -0300

    Exercise 12

--- exercises/bloco13/dia_2/comprehension-exercises-router-master/src/App.js
+++ exercises/bloco13/dia_2/comprehension-exercises-router-master/src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/about'>About</Link></li>
           <li><Link to='/users'>Users</Link></li>
+          <li><Link to='/strict-acess'>Strict Access</Link></li>
         </ul>
         <Switch>
           <Route 

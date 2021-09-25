a73b1e9530199cd35f7d281f14d8cff52021d1c0
jarana-2020 <julioaranabr@yahoo.com.br>
2021-09-25 14:59:56 -0300

    Example 1

--- exercises/bloco15/dia_1/testes-react/src/App.js
+++ exercises/bloco15/dia_1/testes-react/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      saveEmail: '',
+    }
+  }
+
+changeEmail = (value) => {
+  this.setState({email: value});
 }
 
+changeSaveEmail = (value) => {
+  this.setState({saveEmail: value, email: ''})
+}
+
+  render() {
+    const {email, saveEmail} = this.state;
+    return (
+      <div className="App">
+        <label htmlFor="id-e-mail">
+          Email
+          <input 
+          id="id-e-mail"
+          value={email}
+          type="email"
+          onChange= {(e) => this.changeEmail(e.target.value)} />
+        </label>
+        <input 
+          type="button"
+          id="btn-enviar"
+          data-testid="id-send"
+          value="Enviar"
+          onClick={() => this.changeSaveEmail(email)}
+          />
+        <input type="button" id="btn-id" value="Voltar" />
+        <h2 data-testid="id-email-user">{`Valor: ${saveEmail}`}</h2>
+      </div>
+    )
+  }
+}
 export default App;
--- exercises/bloco15/dia_1/testes-react/src/App.test.js
+++ exercises/bloco15/dia_1/testes-react/src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Verifica se existe um input de email na tela', () => {
+  // Acessar os elementos da tela
+  const {getByLabelText} = render(<App />)
+  const inputEmail = getByLabelText('Email')
+  
+  // Fezer teste
+  expect(inputEmail).toBeInTheDocument()
+  expect(inputEmail.type).toBe('email')
 });

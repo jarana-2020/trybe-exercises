4658e6472ceec4245cbbd1f68dfce8ea72a0da57
jarana-2020 <julioaranabr@yahoo.com.br>
2021-09-28 15:07:26 -0300

    text example of the entire application

--- exercises/bloco15/dia_3/testes-route-react/src/App.test.js
+++ exercises/bloco15/dia_3/testes-route-react/src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
+import React from 'react'
 import App from './App';
+import renderWithRouter from './renderWithRouter';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('teste da aplicação toda', () => {
+  it('deve renderizar o componente App', () => {
+    const {getByText} = renderWithRouter(<App />)
+    const home = getByText(/Você está na pagina de Início/)
+    expect(home).toBeInTheDocument()
+  });
+  
+  it('Deve renderizar o componente Sobre', () => {
+    const {getByText, history} = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Sobre/i));
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/about')
+    const aboutAll = getByText(/Você está na página Sobre/);
+    expect(aboutAll).toBeInTheDocument()
+  })
+
+  it('Deve renderizar o componente NotFound', () => {
+    const{getByText, history} = renderWithRouter(<App />);
+    history.push('/pagina/que não existe/');
+    const notFound = getByText(/Página não encontrada/i);
+    expect(notFound).toBeInTheDocument();
+  })
+})
+

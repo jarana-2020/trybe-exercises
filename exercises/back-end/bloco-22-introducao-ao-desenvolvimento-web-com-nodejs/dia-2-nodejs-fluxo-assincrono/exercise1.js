327c83df0734bd05c373565a569bab5e58dc937e
Julio <julioaranabr@yahoo.com.br>
2022-01-10 20:26:07 -0300

    exercise 1

---
+++ exercises/back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercise1.js
@@ -0,0 +1,23 @@
+const calcNumbers = (num1,num2,num3) => {
+  const promisse = new Promise((resolve,reject) =>{
+    if(typeof num1 != "number" || typeof num2 != "number" 
+      || typeof num3 != "number") {
+        reject('Informe apenas números');
+    }
+
+    const result = ((num1 + num2) * num3);
+
+    if(result < 50) reject('Valor muito baixo');
+
+    resolve(result);
+  })
+
+  return promisse;
+};
+
+calcNumbers(10,10,2)
+  .then( result => console.log(result))
+  .catch(error => console.log(error))
+
+ 
+

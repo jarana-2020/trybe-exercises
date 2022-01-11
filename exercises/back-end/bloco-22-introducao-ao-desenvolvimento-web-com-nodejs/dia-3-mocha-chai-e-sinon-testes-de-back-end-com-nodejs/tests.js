e6ec37aad369519afe9c429d73fffa96c07b4261
Julio <julioaranabr@yahoo.com.br>
2022-01-11 18:59:18 -0300

    created test exercise 1

---
+++ exercises/back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/tests.js
@@ -0,0 +1,24 @@
+const { expect } = require("chai");
+
+
+
+describe('Quando o parametro for maior que 0', () => {
+ it('retorna positivo',() => {
+  const answer = verifyNumber(4);
+  expect(answer).to.be.equals('positivo');
+ })
+});
+
+describe('Quando o parametro for menor que 0', () => {
+  it('retorna negativo',() => {
+    const answer = verifyNumber(-2);
+    expect(answer).to.be.equals('negativo');
+ })
+});
+
+describe('Quando o parametro for 0', () => {
+  it('retorna neutro',() => {
+    const answer = verifyNumber(0);
+    expect(answer).to.be.equals('neutro');
+  })
+});

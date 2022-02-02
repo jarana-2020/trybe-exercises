552315cbc978ee8833a02c6f488bb8ba2df1b8f8
Julio <julioaranabr@yahoo.com.br>
2022-02-01 22:20:33 -0300

    refatored controller and service

--- exercises/back-end/bloco-24-nodejs-orm-autenticacao/dia1-introducao-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/controllers/Book.js
+++ exercises/back-end/bloco-24-nodejs-orm-autenticacao/dia1-introducao-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/controllers/Book.js
@@ -3,7 +3,7 @@ const BookService = require('../services/Book');
 const getAllBooks = (async(req,res) => {
   try {
     const books = await BookService.getAllBooks();
-    return books;
+    return res.status(200).json(books)
   } catch (error) {
     res.status(500).json(error);
   }

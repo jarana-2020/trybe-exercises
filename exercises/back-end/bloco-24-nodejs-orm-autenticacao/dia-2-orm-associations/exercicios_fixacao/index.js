4e9e3c884575981b25663300d35e01f232fd4b4b
Julio <julioaranabr@yahoo.com.br>
2022-02-02 12:18:05 -0300

    added Unmanaged transactions

--- exercises/back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exercicios_fixacao/index.js
+++ exercises/back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exercicios_fixacao/index.js
@@ -1,9 +1,16 @@
 // index.js
 const express = require('express');
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
 const { Adress, Employee } = require('./models');
 const { Book, User } = require('./models');
 
+const config = require('./config/config');
+
 const app = express();
+app.use(bodyParser.json());
+
+const sequelize = new Sequelize(config.development);
 
 app.get('/employees', async (_req, res) => {
   try {
@@ -58,6 +65,37 @@ app.get('/usersbooks/:id', async (req, res) => {
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
   };
+});
+
+app.post('/employees', async (req, res) => {
+  // Primeiro iniciamos a transação
+  const t = await sequelize.transaction();
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction: t }, 
+    );
+
+    await Adress.create(
+      { city, street, number, employeeId: employee.id },
+      { transaction: t }, 
+    );
+
+    // Se chegou até essa linha, quer dizer que nenhum erro ocorreu.
+    // Com isso, podemos finalizar a transação usando a função `commit`.
+    await t.commit();
+
+    return res.status(201).json({ message: 'Cadastrado com sucesso' });
+  } catch (e) {
+    // Se entrou nesse bloco é porque alguma operação falhou.
+    // Nesse caso, o sequelize irá reverter as operações anteriores 
+    // com a função rollback, não sendo necessário fazer manualmente
+    await t.rollback();
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
 });
 
 const PORT = process.env.PORT;

927a6f5ee8b0f25ab806570d1249cf70c47a1928
Julio <julioaranabr@yahoo.com.br>
2022-05-25 19:26:54 -0300

    exercise 1

---
+++ exercises/ciencia-computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/exercise1.py
@@ -0,0 +1,22 @@
+#  Faça um programa que receba um nome e imprima
+# o mesmo na vertical em escada invertida.
+
+
+def vertical_iverted(name):
+    word_length = len(name)
+    for index in range(len(name)):
+        print(name[0 : word_length - index])
+
+
+def vertical_name(name):
+    for delete_letter in range(len(name)):
+        for index in range(len(name) - delete_letter):
+            print(name[index], end="")
+        print()
+
+
+# vertical_name("ana")
+# vertical_iverted("julio")
+
+name = input('Informe o nome')
+vertical_iverted(name)
